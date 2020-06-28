40523bb78bec045fdbe365420c5f2075af4dfaba
Zhiwei Liang <zhiwei.liang@asu.edu>
2020-06-27 17:30:35 -0700

    Get things done.

--- setup.py
+++ setup.py
@@ -1,8 +1,3 @@
 import setuptools
 
-setuptools.setup(    
-    name = "test_project",
-    version = 1.0,
-    packages = setuptools.find_packages(),
-    install_requires = ["numpy"]
-)
+setuptools.setup()
